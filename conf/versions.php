5bca5752a0fa3995d06a3c415154f51b51abcb3a
Jason F. Irwin <jason@j2fi.net>
2020-10-10 01:41:08 +0900

    20J102
    Update Includes:
    * fix for new Site 404 errors

--- conf/versions.php
+++ conf/versions.php
@@ -4,7 +4,7 @@
  * @author Jason F. Irwin
  */
 
-define('APP_VER', '20J100');                                            // The Application Version
+define('APP_VER', '20J102');                                            // The Application Version
 define('CSS_VER', '229');                                               // The CSS Update Version
 define('SQL_VER', '001');                                               // The SQL Update Version
 define('SQL_HASH', '39.385|e2c4db08-2651-0170-0045-43c08656bde2');      // The Hash Expected of the Database
