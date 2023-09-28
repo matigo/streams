8b490c4420442ce3a8fba97c33ab9af3494782f4
Jason F. Irwin <jason@matigo.ca>
2023-09-28 10:15:19 +0900

    23I281
    Update Includes:
    * another small CSS adjustment on the Murasaki social theme

--- conf/versions.php
+++ conf/versions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_VER', '23I280');                                            // The Application Version
+define('APP_VER', '23I281');                                            // The Application Version
 define('GENERATOR', 'Streams v1.0');                                    // Generator Name
 
 ?>
